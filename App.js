1eb93eecab2a0132f07a1f686b7b5b43c2d003d9
Mary <marydiva201170@gmail.com>
2024-01-12 10:23:26 +0100

    notif

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react'; 
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Button } from 'react-native-web';
 
+function CategoriesScreen(props) {
+  return (
+    <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Categories</Text>
+      <Button title='ACCESSOIRES' onPress={() => {props.navigation.navigate('Souscategories')}}/>
+      <Button title='PERRUQUES' onPress={() => {props.navigation.navigate('Souscategories')}}/>
+      <Button title='TISSAGES BRÉSILIENS' onPress={() => {props.navigation.navigate('Souscategories')}}/>
+      
+
+    </View>
+  )
+};
+
+function SouscategoriesScreen(props) {
+  return (
+    <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Souscategories</Text>
+      <Button title='ACCESSOIRES' onPress={() => {props.navigation.navigate('')}}/>
+      <Button title='PERRUQUES' onPress={() => {props.navigation.navigate('')}}/>
+      <Button title='TISSAGES BRÉSILIENS' onPress={() => {props.navigation.navigate('')}}/>
+    </View>
+  )
+};
+
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name='Categories' component={CategoriesScreen}/>
+      <Stack.Screen name='Souscategories' component={SouscategoriesScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
